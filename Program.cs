685e52ae843de19a341918e4c056862450dbc9d9
Студент ТУЗ <Student@MyKMK.kulmk.ru>
2022-02-04 11:31:49 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Smirnovbelgit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte x = 10;
+            byte y = 5;
+            y += x;
+            Console.WriteLine(y);
+        }
+    }
+}
